bab949face79873c4b24779384b237f1ad78f3e0
MuhdHanish <muhammedhanish11@gmail.com>
2024-07-20 11:52:56 +0530

    refactor: updated TODO comment in home page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
-      {/* TODO: OTP Verification */}
+      {/* TODO: OTP Verification | PasskeyModal */}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
