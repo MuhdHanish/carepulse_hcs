747490d00496364e14adedcd8ae87178d9809d4e
MuhdHanish <muhammedhanish11@gmail.com>
2024-07-20 15:11:58 +0530

    feat: add EFormFieldType enum to type/index.ts

---
+++ types/index.ts
@@ -0,0 +1,9 @@
+export enum EFormFieldType {
+    INPUT = "input",
+    TEXTAREA = "textarea",
+    PHONE_INPUT = "phoneInput",
+    CHECKBOX = "checkbox",
+    DATE_PICKER = "datePicker",
+    SELECT = "select",
+    SKELETON = "skeleton"
+}
