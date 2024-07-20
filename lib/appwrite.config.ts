e4b8749bccef79baca4cdeb669f3a7e1f3301fc5
MuhdHanish <muhammedhanish11@gmail.com>
2024-07-20 17:16:52 +0530

    feat: created appwriteClient & exported database, storage, messaging and users using appwriteSdk client

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -1,4 +1,6 @@
-const {
+import * as appwriteSdk from "node-appwrite";
+
+export const {
   PROJECT_ID,
   API_KEY,
   DATABASE_ID,
@@ -8,3 +10,17 @@ const {
   NEXT_PUBLIC_STORAGE_BUCKET_ID: BUCKET_ID,
   NEXT_PUBLIC_ENDPOINT: ENDPOINT
 } = process.env;
+
+const appwriteClient = new appwriteSdk.Client();
+
+appwriteClient
+  .setEndpoint(ENDPOINT!)
+  .setProject(PROJECT_ID!)
+  .setKey(API_KEY!);
+
+const database = new appwriteSdk.Databases(appwriteClient);
+const storage = new appwriteSdk.Storage(appwriteClient);
+const messaging = new appwriteSdk.Messaging(appwriteClient);
+const users = new appwriteSdk.Users(appwriteClient);
+
+export { database, storage, users, messaging };
