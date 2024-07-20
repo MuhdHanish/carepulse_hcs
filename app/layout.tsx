d1486542a58540884bcdac3f1ac679b918684a02
MuhdHanish <muhammedhanish11@gmail.com>
2024-07-20 08:58:41 +0530

    style: updated layout.tsx updated font to Plus_Jakarta_Sans imported cn fron utils updated configs of Plus_Jakarta_Sans added basic class to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-sans"
+ });
 
 export const metadata: Metadata = {
   title: "CarePulse",
@@ -16,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}>{children}</body>
     </html>
   );
 }
