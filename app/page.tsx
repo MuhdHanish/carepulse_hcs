6b849f49682b4332ec7a3e695fe738bea06b09f0
MuhdHanish <muhammedhanish11@gmail.com>
2024-07-20 09:53:26 +0530

    chore: add TODO comments in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
+      {/* TODO: OTP Verification */}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
