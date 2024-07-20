9a01c7414d0436758f68dd3f1d6e156c68f6eea5
MuhdHanish <muhammedhanish11@gmail.com>
2024-07-20 15:12:16 +0530

    feat: created coustom field component

---
+++ components/custom-form-field.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { ReactNode } from "react";
+
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "./ui/input";
+import { Control } from "react-hook-form";
+import { EFormFieldType } from "@/types";
+import Image from "next/image";
+
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
+
+type TCustomFormFieldProps = {
+  control: Control<any>;
+  fieldType: EFormFieldType;
+  name: string;
+  label?: string;
+  placeholder?: string;
+  iconSrc?: string;
+  iconAlt?: string;
+  disabled?: boolean;
+  dateFormat?: 'MM/DD/YYYY' | 'DD/MM/YYYY' | 'YYYY-MM-DD' | string;
+  showTimeSelect?: boolean;
+  children?: ReactNode;
+  renderSkeleton?: (field: any) => ReactNode;
+};
+
+const RenderField = ({ field, props }: { field: any; props: TCustomFormFieldProps }) => {
+  const { name, fieldType, placeholder, iconSrc, iconAlt } = props;
+  switch (fieldType) {
+    case EFormFieldType.INPUT:
+      return (
+        <div className="flex rounded-md border border-dark-500 bg-dark-400">
+          {iconSrc && (
+            <Image
+              src={iconSrc}
+              alt={iconAlt || `${name}-icon`}
+              height={24}
+              width={24}
+              className="ml-2"
+            />
+          )}
+          <FormControl>
+            <Input
+              placeholder={placeholder}
+              {...field}
+              className="shad-input border-0"
+            />
+          </FormControl>
+        </div>
+      )
+    case EFormFieldType.PHONE_INPUT:
+      return (
+        <FormControl>
+          <PhoneInput
+            placeholder={placeholder}
+            defaultCountry="US"
+            {...field}
+            international
+            className="input-phone"
+          />
+        </FormControl>
+      )
+    default:
+      break;
+  }
+}
+
+export const CustomFormField = (props: TCustomFormFieldProps) => {
+  const { control, fieldType, name, label } = props;
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex-1">
+          {fieldType !== EFormFieldType.CHECKBOX && label && <FormLabel>{label}</FormLabel>}
+          <RenderField field={field} props={props} />
+          <FormMessage className="shad-error" />
+        </FormItem>
+      )}
+    />
+  );
+};
