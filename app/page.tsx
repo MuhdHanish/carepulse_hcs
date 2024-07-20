5cd79bfd46323c92d8ed75255aa6950fc7fbf1c2
MuhdHanish <muhammedhanish11@gmail.com>
2024-07-20 09:04:28 +0530

    style: add text-white to h1 tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl underline">Home</h1>
+      <h1 className="text-3xl underline text-white">Home</h1>
     </div>
   );
 }
