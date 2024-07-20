da75b47c7bf0f1388264b8cc036daaa1d069fdb2
MuhdHanish <muhammedhanish11@gmail.com>
2024-07-20 09:19:57 +0530

    feat: add ThemeProvider to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,13 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const fontSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
   variable: "--font-sans"
- });
+});
 
 export const metadata: Metadata = {
   title: "CarePulse",
@@ -21,7 +22,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}>{children}</body>
+      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
