588b4457a2b06ee37c11778f525b84b8263309a7
MuhdHanish <muhammedhanish11@gmail.com>
2024-07-20 09:46:23 +0530

    feat: added simple secton for admin

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import { PatientForm } from "@/components/forms/patient-form";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -12,6 +14,11 @@ export default function Home() {
             alt="paitent"
             className="mb-12 h-10 w-fit"
           />
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">© 2024 CarePluse</p>
+            <Link href={`/?admin=true`} className="text-green-500">Admin</Link>
+          </div>
         </div>
       </section>
     </div>
