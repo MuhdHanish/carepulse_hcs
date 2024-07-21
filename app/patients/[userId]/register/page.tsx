00377db586426b23088cb3dc00e5a5147f94af6b
MuhdHanish <muhammedhanish11@gmail.com>
2024-07-21 14:16:41 +0530

    style: updated register page style

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,14 +1,13 @@
 import { RegisterForm } from "@/components/forms/register-form";
 import { getUser } from "@/lib/actions/patient.actions";
 import Image from "next/image";
-import Link from "next/link";
 
 export default async function Register({ params: { userId } }: SearchParamProps) {
   const user = await getUser(userId);
   return (
     <div className="flex h-screen max-h-screen">
-      <section className="remove-scrollbar container my-auto">
-        <div className="sub-container max-w-[496px]">
+      <section className="remove-scrollbar container">
+        <div className="sub-container max-w-[890px] flex-1 flex-col py-10">
           <Image
             src={`/assets/icons/logo-full.svg`}
             height={1000}
@@ -17,10 +16,7 @@ export default async function Register({ params: { userId } }: SearchParamProps)
             className="mb-12 h-10 w-fit"
           />
           <RegisterForm user={user}/>
-          <div className="text-14-regular mt-20 flex justify-between">
-            <p className="justify-items-end text-dark-600 xl:text-left">© 2024 CarePluse</p>
-            <Link href={`/?admin=true`} className="text-green-500">Admin</Link>
-          </div>
+          <p className="copyright py-12">© 2024 CarePluse</p>
         </div>
       </section>
       <Image
