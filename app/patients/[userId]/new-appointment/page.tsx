c387fd6b7ac77c2d6341225abbeafc8e68ecb91d
MuhdHanish <muhammedhanish11@gmail.com>
2024-07-21 17:43:47 +0530

    style: updated style by changing classes in New Appointment page

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function NewApponitment() {
   return (
     <div className="flex h-screen max-h-screen">
-      <section className="remove-scrollbar container">
-        <div className="sub-container max-w-[890px] flex-1 flex-col py-10">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
           <Image
             src={`/assets/icons/logo-full.svg`}
             height={1000}
@@ -20,7 +20,7 @@ export default function NewApponitment() {
         height={1000}
         width={1000}
         alt="appointment-side-image"
-        className="side-img max-w-[390px]"
+        className="side-img max-w-[390px] bg-bottom"
       />
     </div>
   );
