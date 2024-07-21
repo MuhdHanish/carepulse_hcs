0fc356fc1ce541b001ad71b9aea72c8be760b11c
MuhdHanish <muhammedhanish11@gmail.com>
2024-07-21 13:54:09 +0530

    feat: created file uploader component using react-dropzone

---
+++ components/file-uploader.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { convertFileToUrl } from "@/lib/utils";
+import Image from "next/image";
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
+type TFileUploaderProps = {
+  files: File[] | undefined;
+  onChange: (files: File[]) => void;
+};
+
+export const FileUploader = ({ files, onChange }: TFileUploaderProps) => {
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    onChange(acceptedFiles);
+  }, []);
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()} className="file-upload">
+      <input {...getInputProps()} />
+      {files && files?.length > 0 ? (
+        <Image
+          src={convertFileToUrl(files[0])}
+          width={1000}
+          height={1000}
+          alt="uploaded-image"
+          className="max-h-400 overflow-hidden object-cover"
+        />
+      ) : (
+        <>
+          <Image
+            src={`/assets/icons/upload.svg`}
+            width={40}
+            height={40}
+            alt="upload-icon"
+          />
+          <div className="file-upload_label">
+            <p className="text-14-regular">
+              <span className="text-green-500">Click to upload</span>
+              or drag and drop
+            </p>
+            <p>SVG, PNG, JPG or Gif (max 800x400)</p>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
