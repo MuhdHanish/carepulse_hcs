0e1a6119158a6936506a6984ee5848c5be392beb
MuhdHanish <muhammedhanish11@gmail.com>
2024-07-21 20:07:13 +0530

    style: updated padding in home page

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
             className="mb-12 h-10 w-fit"
           />
           <PatientForm />
-          <div className="text-14-regular mt-20 flex justify-between">
+          <div className="text-14-regular py-12 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">© 2024 CarePluse</p>
             <Link href={`/?admin=true`} className="text-green-500">Admin</Link>
           </div>
