3c615d7883a82af840b7a745c678338b43ec614e
MuhdHanish <muhammedhanish11@gmail.com>
2024-07-22 12:23:08 +0530

    feat: created the getRecentAppointments action

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -8,6 +8,7 @@ import {
 } from "../appwrite.config";
 import { parseStringify } from "../utils";
 import { Appointment } from "@/types/appwrite.types";
+import { TStatsCardProp } from "@/components/cards/stats-card";
 
 export const createAppointment = async ({ ...appointmentData }: CreateAppointmentParams): Promise<Appointment> => {
   try {
@@ -33,5 +34,43 @@ export const getAppointment = async (appointmentId: string): Promise<Appointment
     return parseStringify(appointment);
   } catch (error: any) {
     throw new Error(`An error occurred while retrieving the appointment details: ${error.message}`);
+  }
+}
+
+type TInitialCounts = {
+  [K in TStatsCardProp["type"]]: number;
+};
+
+export type TGetRecentAppointmentsReturn = {
+  total: number,
+  appointments: Appointment[]
+} & TInitialCounts;
+
+export const getRecentAppointmentsList = async (): Promise<TGetRecentAppointmentsReturn> => {
+  try {
+    const { documents, total }: { documents: Appointment[]; total: number; } = await database.listDocuments(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      [Query.orderDesc('$createdAt')]
+    );
+    const initialCounts: TInitialCounts = {
+      scheduled: 0,
+      pending: 0,
+      cancelled: 0
+    };
+    const counts = documents?.reduce((acc, appointment) => {
+      if (appointment?.status === "pending") acc.pending += 1;
+      else if (appointment?.status === "scheduled") acc.scheduled += 1;
+      else if (appointment?.status === "cancelled") acc.cancelled += 1;
+      return acc;
+    }, initialCounts);
+    const data = {
+      total,
+      appointments: documents,
+      ...counts
+    };
+    return parseStringify(data);
+  } catch (error: any) {
+    throw new Error(`An error occurred while retrieving the recent appointments details: ${error.message}`);
   }
 }
