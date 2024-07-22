9d2e994754e2b2e1c34d94ecead8d56dd75e563c
MuhdHanish <muhammedhanish11@gmail.com>
2024-07-22 08:47:15 +0530

    feat: added getAppointment func in appointment actions

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import {
   DATABASE_ID,
   APPOINTMENT_COLLECTION_ID,
@@ -21,4 +21,17 @@ export const createAppointment = async ({ ...appointmentData }: CreateAppointmen
   } catch (error: any) {
     throw new Error(`An error occurred while creating a new appointment: ${error.message}`);
   }
-};
+}
+
+export const getAppointment = async (appointmentId: string) => {
+  try {
+    const appointment = await database.getDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId
+    );
+    return parseStringify(appointment);
+  } catch (error: any) {
+    throw new Error(`An error occurred while retrieving the appointment details: ${error.message}`);
+  }
+}
