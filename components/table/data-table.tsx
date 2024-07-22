1fdbb4ba00d76ab0f9769fbdd5837f8e6538cae5
MuhdHanish <muhammedhanish11@gmail.com>
2024-07-22 16:20:20 +0530

    style: updated the proper data-table style

--- components/table/data-table.tsx
+++ components/table/data-table.tsx
@@ -17,6 +17,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -35,12 +36,11 @@ export function DataTable<TData, TValue>({
   });
 
   return (
-    <div>
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
+    <div className="data-table">
+      <Table className="shad-table">
+        <TableHeader className="bg-dark-200">
           {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
+            <TableRow key={headerGroup.id} className="shad-table-row-header">
               {headerGroup.headers.map((header) => {
                 return (
                   <TableHead key={header.id}>
@@ -62,6 +62,7 @@ export function DataTable<TData, TValue>({
               <TableRow
                 key={row.id}
                 data-state={row.getIsSelected() && "selected"}
+                className="shad-table-row"
               >
                 {row.getVisibleCells().map((cell) => (
                   <TableCell key={cell.id}>
@@ -71,7 +72,7 @@ export function DataTable<TData, TValue>({
               </TableRow>
             ))
           ) : (
-            <TableRow>
+            <TableRow className="shad-table-row">
               <TableCell colSpan={columns.length} className="h-24 text-center">
                 No results.
               </TableCell>
@@ -79,43 +80,37 @@ export function DataTable<TData, TValue>({
           )}
         </TableBody>
         </Table>
-        {/* <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="table-actions">
           <Button
             variant="outline"
             size="sm"
             onClick={() => table.previousPage()}
             disabled={!table.getCanPreviousPage()}
+            className="shad-gray-btn"
           >
-            Previous
+          <Image
+            src={`/assets/icons/arrow.svg`}
+            width={24}
+            height={24}
+            alt="arrow-left-icon"
+          />
           </Button>
           <Button
             variant="outline"
             size="sm"
             onClick={() => table.nextPage()}
             disabled={!table.getCanNextPage()}
+            className="shad-gray-btn"
           >
-            Next
+          <Image
+            src={`/assets/icons/arrow.svg`}
+            width={24}
+            height={24}
+            alt="arrow-right-icon"
+            className="rotate-180"
+          />
           </Button>
-        </div> */}
-    </div>
-    {/* <div className="flex items-center justify-end space-x-2 py-4">
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
-        >
-          Previous
-        </Button>
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
-        >
-          Next
-        </Button>
-    </div> */}
+        </div>
     </div>
   );
 }
