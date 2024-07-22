a7b04d00276a9a61e77f27319b2f8fa7d1eba1f1
MuhdHanish <muhammedhanish11@gmail.com>
2024-07-22 11:56:55 +0530

    feat: created stats-card component

---
+++ components/cards/stats-card.tsx
@@ -0,0 +1,31 @@
+import clsx from "clsx";
+import Image from "next/image";
+
+export type TStatsCardProp = {
+  type: "scheduled" | "pending" | "cancelled";
+  count: number;
+  label: string;
+  icon: string;
+};
+
+export const StatsCard = ({ type, count = 0, label, icon }: TStatsCardProp) => {
+  return (
+    <div className={clsx("stat-card", {
+      "bg-appointments": type === "scheduled",
+      "bg-pending": type === "pending",
+      "bg-cancelled": type === "cancelled"
+    })}>
+      <div className="flex items-center gap-4">
+      <Image
+        src={icon}
+        height={32}
+        width={32}
+        alt={`${label}-icon`}
+        className="size-8 w-fit"
+        />
+        <h2 className="text-32-bold text-white">{count}</h2>
+      </div>
+      <p className="text-14-regular">{label}</p>
+    </div>
+  );
+}
