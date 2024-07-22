718a2ef0f9d1f581d6e32b4c5a38d6307a4f05c2
MuhdHanish <muhammedhanish11@gmail.com>
2024-07-22 10:30:00 +0530

    feat: completed the todo to add the passkey modal if admin

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { PatientForm } from "@/components/forms/patient-form";
+import { PasskeyModal } from "@/components/pass-key-modal";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams?.admin === "true";
   return (
     <div className="flex h-screen max-h-screen">
-      {/* TODO: OTP Verification | PasskeyModal */}
+      {isAdmin && <PasskeyModal/>}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
