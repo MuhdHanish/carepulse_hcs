f22ca292134a73d94cf57a91bf662b903ed331db
MuhdHanish <muhammedhanish11@gmail.com>
2024-07-27 19:44:19 +0530

    refactor: updated nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    typescript: {
+         ignoreBuildErrors: true
+    },
+    eslint: {
+        ignoreDuringBuilds: true
+    },
     images: {
         unoptimized: true
     }
