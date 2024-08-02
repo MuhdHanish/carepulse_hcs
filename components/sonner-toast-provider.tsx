6b33a8e3cf90f868a6a97220815ffdb881b6a7ea
MuhdHanish <muhammedhanish11@gmail.com>
2024-08-02 20:02:42 +0530

    feat: added font style in sonner

--- components/sonner-toast-provider.tsx
+++ components/sonner-toast-provider.tsx
@@ -3,5 +3,9 @@
 import { Toaster } from "sonner";
 
 export const SonnerToastProiver = () => {
-    return <Toaster position="top-right" />;
+    return <Toaster
+        position="top-right"
+        visibleToasts={1}
+        style={{ fontFamily: "Plus Jakarta Sans, sans-serif" }}
+    />;
 }
