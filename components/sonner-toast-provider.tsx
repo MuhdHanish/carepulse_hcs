bc712dd000114aab40d6c5494c8535df27e5d3cd
MuhdHanish <muhammedhanish11@gmail.com>
2024-08-05 17:37:52 +0530

    style: changed sonner theme to black

--- components/sonner-toast-provider.tsx
+++ components/sonner-toast-provider.tsx
@@ -6,6 +6,7 @@ export const SonnerToastProiver = () => {
     return <Toaster
         position="top-right"
         visibleToasts={1}
+        theme="dark"
         style={{ fontFamily: "Plus Jakarta Sans, sans-serif" }}
     />;
 }
